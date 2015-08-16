ad324f2de50fe06536652e1cd82c7ffa273ac2a2
Kashav Madan <madanballer@hotmail.com>
2015-08-16 16:01:27 -0400

    Restructured front-end to work with Angular 1.4.4

--- public/app.js
+++ public/app.js
@@ -1,34 +1,32 @@
-var app = angular.module('app', []);
+var app = angular.module('app', [])
 
-function mainController($scope, $http) {
-	$scope.formData = {};
-	$http.get('/api/sites')
-		.success(function(data){
-			$scope.sites = data;
-			console.log(data);
-		})
-		.error(function(data) {
-			console.log('Error: '+data);
-		});
-	$scope.addSite = function() {
-		$http.post('/api/sites', $scope.formData)
+	.controller('mainController', function($http) {
+		var vm = this;
+		vm.formData = {};
+		$http.get('/api/sites')
 			.success(function(data){
-				$scope.formData = {}; // clear form for next site
-				$scope.sites = data;
-				console.log(data);
+				vm.sites = data;
 			})
-			.error(function(data){
+			.error(function(data) {
 				console.log('Error: '+data);
 			});
-	};
-	$scope.removeSite = function(id) {
-		$http.delete('/api/sites'+id) 
-			.success(function(data){
-				$scope.sites = data;
-				console.log(data);
-			})
-			.error(function(data){
-				console.log('Error: ' + data);
-			});
-	};
-}
+		vm.addSite = function() {
+			$http.post('/api/sites', vm.formData)
+				.success(function(data){
+					vm.formData = {}; // clear form for next site
+					vm.sites = data;
+				})
+				.error(function(data){
+					console.log('Error: '+data);
+				});
+		};
+		vm.removeSite = function(id) {
+			$http.delete('/api/sites/'+id)
+				.success(function(data){
+					vm.sites = data;
+				})
+				.error(function(data){
+					console.log('Error: '+data);
+				});
+		};
+	});
