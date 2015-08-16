9f661c9dbf15373848df47e64cbd65113007d1f1
Kashav Madan <madanballer@hotmail.com>
2015-08-16 14:52:01 -0400

    Created Angular module+controller to handle api methods for adding/viewing/removing sites

--- public/app.js
+++ public/app.js
@@ -1,1 +1,34 @@
 var app = angular.module('app', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+	$http.get('/api/sites')
+		.success(function(data){
+			$scope.sites = data;
+			console.log(data);
+		})
+		.error(function(data) {
+			console.log('Error: '+data);
+		});
+	$scope.addSite = function() {
+		$http.post('/api/sites', $scope.formData)
+			.success(function(data){
+				$scope.formData = {}; // clear form for next site
+				$scope.sites = data;
+				console.log(data);
+			})
+			.error(function(data){
+				console.log('Error: '+data);
+			});
+	};
+	$scope.removeSite = function(id) {
+		$http.delete('/api/sites'+id) 
+			.success(function(data){
+				$scope.sites = data;
+				console.log(data);
+			})
+			.error(function(data){
+				console.log('Error: ' + data);
+			});
+	};
+}
